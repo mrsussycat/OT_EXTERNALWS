f9636efefb7fafa4a1c05c2bedc8fc65a12b042f
ruthwik <97641829+mrsussycat@users.noreply.github.com>
2023-07-19 15:23:45 +0530

    Update index.js refactor and modify

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const USERNAME = 'user001';
 const PASSWORD = 'password';
 
 function isAuth(req, res, next) {
-    // const auth = req.headers.authorization;
-    // if (auth === 'password') {
-    //   next();
-    // } else {
-    //   res.status(401);
-    //   res.send('Access forbidden');
-    // }
-
     if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
         return res.status(401).json({ message: 'Missing Authorization Header' });
     }
@@ -20,32 +12,31 @@ function isAuth(req, res, next) {
     const base64Credentials =  req.headers.authorization.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [username, password] = credentials.split(':');
-    
     if (username === USERNAME && password === PASSWORD) {
         next();
     } 
-
     res.status(401).json({response: 'Access Forbidden, NEGATIVE'});
 }
 
-app.patch('/', (req, res) => {
-    res.status(200).json({response: 'POSITIVE'});
-})
 
 app.get('/', (req, res) => {
-    res.status(200).json({response: 'POSITIVE'});
+    res.status(200).json({response: 'GET POSITIVE'});
 })
 
-app.get('/get400', (req, res) => {
-  res.status(400).json({response: 'CODE - 400, POSITIVE'});
+app.post('/', (req, res) => {
+    res.status(200).json({response: 'POST POSITIVE'});
+})
+
+app.patch('/', (req, res) => {
+    res.status(200).json({response: 'PATCH POSITIVE'});
+})
+
+app.get('/get408', (req, res) => {
+  res.status(408).json({response: 'CODE - 408, POSITIVE'});
 })
 
 app.get('/secret', isAuth, (req, res) => {
     res.status(200).json({response: 'AUTH SUCCESSFUL, POSITIVE'});
-})
-
-app.get('/getJson', (req, res) => {
-    res.json({response: 'POSITIVE'});
 })
 
 app.listen(PORT, () => {
