680bf705e1321d3a6e7eff9fcd6e68ba5e1f93a9
Thummurugoti Sai Ruthwik <truthwik@opentext.com>
2024-03-13 10:02:10 +0530

    added timeout

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require('express');
+const querystring = require('querystring');
 const app = express();
-const basicAuth = require('express-basic-auth');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-const users = {
-  'username': 'password',
-};
+app.use(express.json());
 
-app.use(basicAuth({
-  users,
-  challenge: true, // Send a 401 Unauthorized response if credentials are not provided
-  unauthorizedResponse: 'Unauthorized Access', // Customize the response for unauthorized requests
-}));
+app.get('/search', (req, res) => {
+  const queryParams = querystring.parse(req.url.split('?')[1]);
+  console.log(queryParams);
+  const date = queryParams.date;
 
-app.get('/protected', (req, res) => {
-  res.send('Access Granted');
+  if (date) {
+    console.log(`Date query: ${date}`);
+    res.send({ "Date": date });
+  } else {
+    console.log(`Date query: ${date}`);
+    res.send({ "Invalid Date": "Invalid Date" });
+  }
 });
+
+app.post('/data', (req, res) => {
+  console.log(req.body);
+  res.send({"response": req.body});
+})
+
+app.get('/:timeoutValue', (req, res) => {
+  const timeoutValue = parseInt(req.params.timeoutValue, 10);
+  setTimeout(() => {
+    res.status(200).json({timeoutValue: timeoutValue});
+  }, timeoutValue * 1000)
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-})
+});
